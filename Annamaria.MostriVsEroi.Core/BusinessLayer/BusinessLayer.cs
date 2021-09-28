73d488ce30c694bf794aff7a75be54d26f147535
Annamaria Ciasca <annamaria.ciasca@avanade.com>
2021-09-28 16:15:06 +0200

    Aggiunto BusinessLayer

---
+++ Annamaria.MostriVsEroi.Core/BusinessLayer/BusinessLayer.cs
@@ -0,0 +1,28 @@
+﻿using Annamaria.MostriVsEroi.Core.RepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annamaria.MostriVsEroi.Core.BusinessLayer
+{
+   public class BusinessLayer: IBusinessLayer
+    {
+        private readonly IRepositoryArmi armiRep;
+        private readonly IRepositoryCategorie categorieRep;
+        private readonly IRepositoryEroi eroiRep;
+        private readonly IRepositoryGiocatori giocatoriRep;
+        private readonly IRepositoryMostri mostriRep;
+
+        public BusinessLayer(IRepositoryArmi armi, IRepositoryCategorie categorie, IRepositoryEroi eroi, IRepositoryGiocatori giocatori, IRepositoryMostri mostri)
+        {
+            armiRep = armi;
+            categorieRep = categorie;
+            eroiRep = eroi;
+            giocatoriRep = giocatori;
+            mostriRep = mostri;
+        }
+
+    }
+}
--- Annamaria.MostriVsEroi/Program.cs
+++ Annamaria.MostriVsEroi/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using Annamaria.MostriVsEroi.Core.BusinessLayer;
+using Annamaria.MostriVsEroi.Mock;
+using System;
 
 namespace Annamaria.MostriVsEroi
 {
     class Program
     {
+        private static readonly IBusinessLayer bl = new BusinessLayer(new RepositoryArmi(), new RepositoryCategorie(), new RepositoryEroi(), new RepositoryGiocatori(), new RepositoryMostri());
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Benvenuto!");
+            MenuPrincipale();
+        }
+
+
+        public static void MenuPrincipale()
+        {
+            bool continua = true;
+            int scelta;
+
+            do
+            {
+                Console.WriteLine("Menu principale\n");
+
+                Console.WriteLine("Digita 1 per accedere");
+                Console.WriteLine("Digita 2 per registrati");
+                Console.WriteLine("Digita 0 per uscire");
+
+                while (!int.TryParse(Console.ReadLine(), out scelta) || scelta < 0 || scelta > 2 )
+                {
+                    Console.WriteLine("Inserire valore corretto!");
+                }
+
+                switch (scelta)
+                {
+                    case 1:
+                        //bl.Accedi();
+                        break;
+                    case 2:
+                       // bl.Registrati();
+                        break;
+                    case 0:
+                        Console.WriteLine("Ciao!");
+                        continua = false;
+                        break;
+                    default:
+                        Console.WriteLine("Scelta errata.");
+                        break;
+                }
+            } while (continua);
         }
     }
-}
+    }
+
